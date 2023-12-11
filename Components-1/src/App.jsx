62411a167a0942a60857d3426a7a47a588d08779
Ruben <rubencirelli4@gmail.com>
2023-12-11 20:25:13 +0100

    components-1

---
+++ Components-1/src/App.jsx
@@ -0,0 +1,10 @@
+import { HelloWorld } from "./HelloWorld";
+
+export function App(){
+    return(
+        <div>
+            <h1>My AWesome Application</h1>
+            <HelloWorld/>
+        </div>
+    )
+}
---
+++ Components-1/src/main.jsx
@@ -0,0 +1,6 @@
+import { App } from "./App"
+import { HelloWorld } from "./HelloWorld"
+import {createRoot} from 'react-dom/client'
+
+createRoot(document.getElementById("root")).render(<App/>)
+
