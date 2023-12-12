851e791036ab1c65d31bbe206b54f2a890ab18a0
Ruben <rubencirelli4@gmail.com>
2023-12-12 13:56:21 +0100

    Conditional_render2

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,10 +2,10 @@ export function Hello({name, age}){
     return (
             <>
             <p>Hello. <strong>{name}</strong>!</p>
-            {!!age && <p>the {age} is rendered</p>}
-            {age>18 && <p>the {age} is greater than 18</p>}
-            {age>18 && age<65  && <p>the {age} is rendered and it is bigger than 18 and less than 65</p>}
-            {age>18 && age<65 && name=="John" && <p>the {age} is rendered and it is bigger than 18 and less than 65 and the name prop is equal to "John".</p>}
+            {!!age && age>18 ? <p>Your age is {age} </p>: <p>You are very young!</p>}
+            {age>65 && <p>You are very OLD!</p>}
+            
+            
             </>
     )
 }
