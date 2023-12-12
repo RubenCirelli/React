9b5baee4b0120f8003920c33682153c1370fe2ee
Ruben <rubencirelli4@gmail.com>
2023-12-12 13:45:49 +0100

    Conditional_render1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export function App(){
     return(
         <>
         {/* <Hello /> lasciando il props non compilato l'output rimane invariato, lasciando solo Hello! */}
-        <Hello name="Ruben" age={22}/>
+        <Hello name="John" age={22}/>
         </>
     )
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,8 +1,11 @@
-export function Hello({name}){
+export function Hello({name, age}){
     return (
             <>
             <p>Hello. <strong>{name}</strong>!</p>
-            <Age age={22}/>
+            {!!age && <p>the {age} is rendered</p>}
+            {age>18 && <p>the {age} is greater than 18</p>}
+            {age>18 && age<65  && <p>the {age} is rendered and it is bigger than 18 and less than 65</p>}
+            {age>18 && age<65 && name=="John" && <p>the {age} is rendered and it is bigger than 18 and less than 65 and the name prop is equal to "John".</p>}
             </>
     )
 }
