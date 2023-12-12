4553340e32ea8c807a80d7da40054fb3963e9c3a
Ruben <rubencirelli4@gmail.com>
2023-12-13 00:22:18 +0100

    Handle-Events2

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,10 +1,13 @@
 export function MouseClick(){
     function handleButtonClick(event){
-        console.log(event.target.name);
+        console.log(event.target.src);
+        //preventing that name gets out as output
+        
     }
     
     return(
         <button name="one" onClick={handleButtonClick}>
+            <img src="abc" alt="photo" />
             click me 
         </button>
     )
