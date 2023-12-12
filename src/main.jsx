baf351f0dacba31f6e5a929ece12416ecdaed2b1
Ruben <rubencirelli4@gmail.com>
2023-12-12 19:19:23 +0100

    State-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import {AlertClock} from "./App";
 import {createRoot} from 'react-dom/client'
+import { Counter } from "./Counter";
 
-createRoot(document.getElementById("root")).render(<AlertClock/> )
+createRoot(document.getElementById("root")).render(<Counter/> )
 //i componente ripetuto come abbiam fatto verrà renderizzato molteplici volte in base
