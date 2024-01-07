791dde556c6cff6d4fc7eae8f1d29f9afe09529d
Ruben <rubencirelli4@gmail.com>
2024-01-07 04:04:16 +0100

    Styling Components 1

--- src/app.jsx
+++ src/app.jsx
@@ -5,11 +5,13 @@ import { FocusableInput } from "./FocusableInput";
 import { ToDoList } from "./ListsAndState";
 import { ToDo } from "./ToDo";
 import { ToDo2 } from "./ToDoProva";
+import { Welcome } from "./Welcome";
 
 
 export function App() {
   return (
     <div>
+      <Welcome name="RU" eta={22} />
       <ToDo2 />
 
     </div>
