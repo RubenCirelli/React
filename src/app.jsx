21243ef03d218b4efcffa949ad3fdd1d3b78dc2d
RubenCirelli <145332442+RubenCirelli@users.noreply.github.com>
2024-01-07 03:39:09 +0100

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
+import { Counter } from "./Counter";
 import { FocusableInput } from "./FocusableInput";
 
 export function App(){
   return(
     <div>
-      <FocusableInput/>
+      <Counter initialValue={0} variable={2}/>
     </div>
   )
 } 
