dab3e0b133bfb73a6b96a073eceab4fc300121b5
Ruben <rubencirelli4@gmail.com>
2024-01-14 21:20:57 +0100

    React Router 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import  FetchData  from "./GitHubUser";
+import { Hello } from './Welcome';
+import { Counter } from "./Counter"
 
   
 const App =() => {
@@ -8,6 +10,8 @@ const App =() => {
         <Router>
             <Routes>
                 <Route path="users/:username" element= {<FetchData username="RubenCirelli" />}></Route>
+                <Route path="/Counter" element= {<Counter />}></Route>
+                <Route path="/" element= {<Hello name="Ruben" />}></Route>
             </Routes>
         </Router>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client'
-import  App  from './app'
+import  App  from './App'
 
 createRoot(document.getElementById("root")).render(<App/> )
 //i componente ripetuto come abbiam fatto verrà renderizzato molteplici volte in base
