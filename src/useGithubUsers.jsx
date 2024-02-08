7105e601027a979c2c2549c925190c4997ff0aa1
Ruben <rubencirelli4@gmail.com>
2024-02-08 18:05:16 +0100

    SWR2

--- src/useGithubUsers.jsx
+++ src/useGithubUsers.jsx
@@ -4,7 +4,8 @@ const fetcher = url => fetch(url).then(response => response.json())
 
 
 export const useGithubUser = (username) => {
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher);
+    const {data, error} = useSWR(username ? `https://api.github.com/users/${username}`: {}, fetcher);
+
 
     return {
         userData: data,
